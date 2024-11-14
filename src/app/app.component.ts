c0a6db90f07b948c541d09f57e226a6f9af693aa
WellingtonTorres <wellingtontorresmelo@gmail.com>
2024-11-14 09:49:39 -0300

    subindo rota de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import {
-  PoMenuItem,
-  PoMenuModule,
-  PoPageModule,
-  PoToolbarModule,
-} from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    PoToolbarModule,
-    PoMenuModule,
-    PoPageModule
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  readonly menus: Array<PoMenuItem> = [
-    { label: 'Home', action: this.onClick.bind(this) },
-  ];
-
-  private onClick() {
-    alert('Clicked in menu item');
-  }
+  title = 'task-list-frontend';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { AuthComponent } from './app/features/auth/auth.component';
-import { provideRouter, Routes } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
-export const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: AuthComponent},
-  {path: '**', redirectTo: 'login'}
-];
-
-
-// Inicialize a aplicação
-bootstrapApplication(AuthComponent, {
-  providers: [
-    provideRouter(routes),                              // Configuração de rotas
-    importProvidersFrom(BrowserAnimationsModule)        // Outros providers necessários
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
